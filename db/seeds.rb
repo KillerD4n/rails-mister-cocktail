e550c0a93e83490a3825bcbcf4fb5f9b3cb873c6
Daniel Blake <daniel.blake1@hotmail.co.uk>
2018-05-17 18:50:19 +0200

    Design added

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,8 @@ Ingredient.create(name: "vodka")
 Ingredient.create(name: "brown sugar")
 Ingredient.create(name: "tonic water")
 Ingredient.create(name: "gin")
+
+Cocktail.create(name: "Moscow Mule")
+Cocktail.create(name: "Margarita")
+Cocktail.create(name: "Long Island Ice Tea")
+Cocktail.create(name: "White Russian")
